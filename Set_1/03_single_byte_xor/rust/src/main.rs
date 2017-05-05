f5934159a71737607a19edd6ec80d31f5970b6a4
sahil <sahilmohdsahil126@gmail.com>
2017-05-05 22:38:56 +0400

    S01T03 python done, rust TODO

--- Set_1/03_single_byte_xor/rust/src/main.rs
+++ Set_1/03_single_byte_xor/rust/src/main.rs
@@ -4,16 +4,12 @@ use serialize::hex::FromHex;
 use serialize::hex::ToHex;
 
 fn main() {
-	let op_1 = "1c0111001f010100061a024b53535009181c";
-	let hex_1 = op_1.from_hex().unwrap();
-	let op_2 = "686974207468652062756c6c277320657965";
-	let hex_2 = op_2.from_hex().unwrap();
+	let encoded = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+	let hex = encoded.from_hex().unwrap();
 
-	let mut res = Vec::new();
 
-	for (i, _) in hex_1.iter().enumerate() {
-		res.push(hex_1[i] ^ hex_2[i]);
+	// TODO
+	for i in &hex {
+		println!("{}", i);	
 	}
-
-	println!("{:?}", res.to_hex());
 }
